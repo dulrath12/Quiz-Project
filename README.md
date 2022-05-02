fb1bb1a1d84fd628c08723a1a9fab23750da9039
Dakotah Ulrath <dulrath@live.com>
2022-05-01 21:37:55 -0400

    highscores page

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Quiz-Project
+
+## Description
+The purpose of the project was to utilize web API functions to create a quiz that was times, and that the highscores could be saved afterwards. I managed to get the quiz created, and the timer to countdown, but upon answering wrong, and minusing the 10 seconds for the wrong answer, it would immediately go back to the original place in the timer. i also could not g
--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,13 +1,25 @@
 var saveScorebtn = document.getElementById('saveScore')
 var username = document.getElementById('username')
-var finalTime = document.getElementById('final-time')
+var scoreEl = document.getElementById('score')
 var mostRecentTime = localStorage.getItem('mostRecentTime')
+var hsbtnEl = document.getElementById('hs-btn')
+var scoresListEl = document.getElementById('score-list')
+var resetbtnEl = document.getElementById('reset-btn')
+var playAgainEl = document.getElementById('play-again')
 
-var highScores = JSON.parse(localStorage.getItem('highscores'))
+var highScores = JSON.parse(localStorage.getItem('highScores')) || []
+
+scoreEl.innerHTML = mostRecentTime
 
 
 saveHighScore = e =>{
     e.preventDefault()
+
+    hsbtnEl.remove();
+    username.remove();
+    scoreEl.remove();
+    
+    
 
     var score = {
         score: mostRecentTime,
@@ -21,5 +33,24 @@ saveHighScore = e =>{
     })
 
     localStorage.setItem('highScores', JSON.stringify(highScores))
-    window.location.assign('/')
+
+    scoresListEl.innerHTML = highScores.map(score => {
+        return '<li>' + score.name + '-' + score.score + "</li>"
+    } )
 }
+
+highScores.sort((a,b) => {
+    return b.score - a.score
+})
+
+scoresListEl.innerHTML = highScores.map(score => {
+    return '<li>' + score.name + '-' + score.score + "</li>"})
+
+scoresListEl.className = "score-list"
+
+function reset(){
+    localStorage.clear();
+
+    window.location.assign("./highscores.html")
+}
+
